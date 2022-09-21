2ccfc44811402e1957dbf9dc7ef49e55845f82d2
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-09-21 22:04:24 +0300

    Done with assignment

--- src/Comment.js
+++ src/Comment.js
@@ -5,3 +5,11 @@ function Comment() {
 }
 
 export default Comment;
+/*
+function Article() {
+  return (
+    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+  );
+}
+export default Article;
+*/
